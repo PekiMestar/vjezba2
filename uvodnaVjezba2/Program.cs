9a12d2ebeafaaffa525f0b4ed2d07842ad082c02
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-09-20 09:08:57 +0200

    Add files via upload

---
+++ uvodnaVjezba2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c, d;
+            double avg = 0;
+
+            a = 10;
+            b = 7;
+
+            c = 12.5;
+            d = 16.7;
+
+            avg = (a + b + c + d) / 4;
+
+            Console.WriteLine("Prosječna vrijednost je " + avg);
+            Console.ReadKey();
+        }
+    }
+}
